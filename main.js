e787dfcb802747939a6689c9ed1f11dc8c18836f
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-07-12 16:39:35 -0400

    vue instance

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue ({
+    el: "#app",
+    data: {
+        product: "Urban Skin RX",
+        description: "This is an amazing skincare product made specifically for people of color. It has done wonders for me."
+    }
+})
