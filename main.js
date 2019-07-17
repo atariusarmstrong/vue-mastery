57b95470179ecfde63a49ec551a31585ae609b74
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-07-17 18:32:08 -0400

    computed properties

--- main.js
+++ main.js
@@ -2,24 +2,25 @@ var app = new Vue ({
     el: "#app",
     data: {
         product: "Socks",
+        brand: "Vue Mastery",
         description: "A nice comfy sock made from wool.",
-        image: "./vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s?k=socks&ref=nb_sb_noss_1",
-        inStock: false,
-        inventory: 0,
         onSale: false,
         details: ["80% cotton", "20% polyester", "Gender neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: './vmSocks-green-onWhite.jpg'
+                variantImage: './vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './vmSocks-blue-onWhite.jpg'
+                variantImage: './vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         sizes: ["XS", "S", "M", "L", "XL", "XXL"],
@@ -29,11 +30,28 @@ var app = new Vue ({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(this.selectedVariant)
         },
         removeFromCart() {
             this.cart -= 1
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            //we're passing this.selectedVariant is the array index
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        productOnSale() {
+            return this.brand + ' ' + this.product + " is now on sale!!!"
+        }
+        
     }
 })
